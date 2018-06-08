3128e28fa0ab256d22d9202d38f71d4cca6b4a2f
JackCummingsIfes <37441942+JackCummingsIfes@users.noreply.github.com>
2018-06-08 18:02:14 -0300

    Update PgAdmin.sql

--- PgAdmin.sql
+++ PgAdmin.sql
@@ -17,50 +17,51 @@ create table Usuario(cpf_usuario varchar(15) primary key,
                      nivel_de_acesso smallint,
                      id_endereco_usuario integer references Endereco(id_endereco));
 
+create table Estabelecimento (cnpj varchar(25) primary key,
+                              nome_estabelecimento text,
+                              endereco_estabelecimento text,
+                              tipo_estabelecimento text,
+                              telefone_estabelecimento varchar(15),
+                              email_estabelecimento text,
+                              horario_estabelecimento time);
 
 create table Limitacoes (id_limitacoes varchar(10) primary key,
                          Saldo_Reserva money,
-                         Data_Reserva date
-			 cpf_limitacoes integer references Usuario(cpf_usuario));
+                         Data_Reserva date,
+			 cpf_limitacoes varchar(15) references Usuario(cpf_usuario));
 
 create table Promocoes (id_promocao varchar(10) primary key,
                         desconto_promocao money,
                         validade_promocao date,
                         tipo_promocao text,
-                        descricao_promocao text
-                        cnpj_promocao integer references Estabelecimento(cnpj));
+                        descricao_promocao text,
+                        cnpj_promocao varchar(25) references Estabelecimento(cnpj),
+                        cpf_promocao varchar(15) references Usuario(cpf_usuario));
 
 create table Produtos (id_produto varchar(20) primary key,
                        Descricao_produto text,
                        Preco_produto money,
                        nome_produto text,
-                       tipo_produto text
-                       cnpj_produto integer references Estabelecimento(cnpj));
+                       tipo_produto text,
+                       cnpj_produto varchar(25) references Estabelecimento(cnpj));
                      
-create table Estabelecimento (cnpj varchar(25) primary key,
-                              nome_estabelecimento text,
-                              id_endereco_estabelecimento integer references Endereco(id_endereco),
-                              tipo_estabelecimento text,
-                              telefone_estabelecimento varchar(15),
-                              email_estabelecimento text,
-                              horario_estabelecimento time );
 
 create table Restricoes (id_restricao varchar(20) primary key,
-                         tempo_duracao date,
                          data_inicio date,
-                         motivo_restricao text
-                         cnpj_restricao integer references Estabelecimento(cnpj),
-                         cpf_restricao integer references Usuario(cpf_usuario));
+                         data_fim date,
+                         motivo_restricao text,
+                         cnpj_restricao varchar(25) references Estabelecimento(cnpj),
+                         cpf_restricao varchar(15) references Usuario(cpf_usuario));
 
 create table Eventos (id_evento varchar(20) primary key,
                       descricao_evento text,
                       data_evento date,
                       preco_evento money,
-                      hora_evento Time
-                      cnpj_restricao integer references Estabelecimento(cnpj));
+                      hora_evento time,
+                      cnpj_restricao varchar(25) references Estabelecimento(cnpj));
 
 create table Alertas (id_alerta varchar(20) primary key,
                       descricao_alerta text,
                       nome_alerta text,
-                      regras_alerta text
-                      cpf_alerta integer references Usuario(cpf_usuario));
+                      regras_alerta text,
+                      cpf_alerta varchar(15) references Usuario(cpf_usuario));
