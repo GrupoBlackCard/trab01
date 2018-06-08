550894701901d8674d6e89f28140c8b20cfd8818
JackCummingsIfes <37441942+JackCummingsIfes@users.noreply.github.com>
2018-06-08 09:32:49 -0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.endereco(estado, rua, cep, bairro, municipio, numero)
+	VALUES ('PB','Rua Hilta Martins de Lima','58069-297','Gramame','João Pessoa',516)
+            ('SE','Rua Evandro Ribeiro','49069-066','Porto DAntas','Aracaju',405);
