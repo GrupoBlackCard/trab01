e9599f8a0bcf51c7d5a290f74e9ef14139e8aa42
JackCummingsIfes <37441942+JackCummingsIfes@users.noreply.github.com>
2018-06-08 16:21:07 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,3 +1,19 @@
-INSERT INTO public.endereco(estado, rua, cep, bairro, municipio, numero)
-	VALUES ('PB','Rua Hilta Martins de Lima','58069-297','Gramame','João Pessoa',516)
-            ('SE','Rua Evandro Ribeiro','49069-066','Porto DAntas','Aracaju',405);
+INSERT INTO Endereco(estado, rua, cep, bairro, municipio, numero) VALUES 
+('PB','Rua Hilta Martins de Lima','58069-297','Gramame','João Pessoa',516),
+('SE','Rua Evandro Ribeiro','49069-066','Porto DAntas','Aracaju',405),
+('MA','Beco Onze','65637-566','Bela Vista','Timon',737),
+('RJ','Rua Oito','25924-370','Parque Recreio Dom Pedro II (Guia de Pacobaíba)','Magé',423),
+('RO','Rua Portuguesa','76812-612','Três Marias','Porto Velho',877),
+('AM','Beco Doutora Telma','69084-151','Zumbi dos Palmares','Manaus',591),
+('PI','Rua Oito','64030-474','Bela Vista','Teresina',125),
+('SC','Rua Osvaldo Cruz','88811-190','Cruzeiro do Sul','Criciúma',593);
+
+INSERT INTO Usuario(cpf_usuario,saldo_usuario,login_usuario,genero_usuario,senha_usuario,celular_usuario,nome_usuario,orientacao_sexual,nivel_de_acesso,id_endereco_usuario) values
+('880.208.195-68',71.44,'matheusruanbarros','M','RWjWnpNult','(48) 98337-8847','Matheus Ruan Barros','Heterosexual',1,1),
+('396.931.337-65',39.35,'emanuellygabrielasouza','F','99Dn4XyNth','(67) 98926-7661','Emanuelly Gabriela Souza','Bisexual',1,2),
+('191.491.029-07',80.54,'luizbenjamingomes','M','old1FZ9Kn6','(67) 99583-2055','Luiz Benjamin Gomes','Bisexual',1,3),
+('402.342.207-08',72.50,'raulfilipegomes_','M','JrEZAJHBHZ','(83) 99179-5936','Raul Filipe Gomes','Homosexual',1,4),
+('693.970.464-70',93.19,'luisheitorgomes','M','WYPqXDuwrN','(85) 98460-2172','Luís Heitor Gomes','Homosexual',1,5),
+('114.023.514-10',6.67,'carolinealiciaelianecavalcanti','F','1PEQQQl0dB','(65) 98395-2419','Caroline Alícia Eliane Cavalcanti','Bisexual',1,6),
+('397.496.435-50',20.96,'ccarloseduardo','M','DQa0wYdNuE','(71) 99125-6194','Carlos Eduardo Heitor Ricardo das Neves','Bisexual',1,7),
+('355.030.089-10',44.33,'ggeraldomarcosgomes','M','dfIEicHkSC','(61) 98331-8515','Geraldo Marcos Gomes','Homosexual',1,8);
